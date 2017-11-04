a4d25c42b0f6bd30bf93a766afa56d837da9579c
sergioeabarcaf <sergioaf1991@gmail.com>
2017-11-04 15:25:21 -0300

    sergioeabarcaf SEPARAR sistemas: Se separaran en dos carpetas los sistemas para una proxima ejecucion

---
+++ alerta/ejecutar.py
@@ -0,0 +1,13 @@
+import os
+import time
+import sys
+
+# Guardar registro de las veces que se reinicia el computador
+f=open('timelog.txt','a')
+f.write(time.strftime("%c") + "\n")
+f.close()
+time.sleep(10)
+# Ejecucion del servidor nodejs, Cambiar el valor de "pi"
+# ubicado en la siguiente ruta, por el usuario de su computadora
+os.system('sudo node /home/pi/nodeCentral/app.js &')
+sys.exit()
