2fedbd0a72f9c38238254566f5a88f9b575b4c65
sergioeabarcaf <sergioaf1991@gmail.com>
2018-01-03 17:02:09 -0300

    Eliminar elementos sobrantes

--- ejecutar.py
+++ ejecutar.py
@@ -9,9 +9,5 @@ f.close()
 time.sleep(10)
 # Ejecucion del servidor nodejs, Cambiar el valor de "pi"
 # ubicado en la siguiente ruta, por el usuario de su computadora
-<<<<<<< HEAD
 os.system('nohup sudo node /home/turismo/nodeCentral/app.js &')
-=======
-os.system('sudo node /home/pi/nodeCentral/app.js &')
->>>>>>> d481d5b1570ba7c1bd28ee049186d95360de5df2
 sys.exit()
--- nodejs/public/js/alert.js
+++
@@ -1,9 +0,0 @@
-function updateAlerta(mensaje){
-  console.log("entro a funcion");
-  var socket = io.connect();
-
-  socket.emit('sendAlert',{
-    value:mensaje
-  });
-  console.log("se envia mensaje de alerta");
-};
