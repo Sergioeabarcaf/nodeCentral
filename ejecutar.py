d37829000c098dc3d5a403e94ecb7cbbb272a735
sergioeabarcaf <sergioaf1991@gmail.com>
2018-01-11 19:13:48 -0300

    cambio en ruta time

--- ejecutar.py
+++ ejecutar.py
@@ -3,10 +3,10 @@ import time
 import sys
 
 # Guardar registro de las veces que se reinicia el computador
-f=open('timelog.txt','a')
+time.sleep(80)
+f=open('/home/turismo/nodeCentral/timelog.txt','a')
 f.write(time.strftime("%c") + "\n")
 f.close()
-
 while (True):
     date = time.strftime("%Y%m%d%T")
     os.system("mosquitto_pub -h 192.168.251.3 -t time -m "+date)
