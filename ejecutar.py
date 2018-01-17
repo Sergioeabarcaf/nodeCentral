7a33ffd8ac5fa39f7f11f3af753ee75445c57347
sergioeabarcaf <sergioaf1991@gmail.com>
2018-01-17 14:48:01 -0300

    se almacena solamente la hora de inicio del sistema con un mensaje, se elimina envio de hora por mqtt

--- ejecutar.py
+++ ejecutar.py
@@ -5,11 +5,6 @@ import sys
 # Guardar registro de las veces que se reinicia el computador
 time.sleep(80)
 f=open('/home/turismo/nodeCentral/timelog.txt','a')
-f.write(time.strftime("%c") + "\n")
+f.write(time.strftime("%c") + " Se acaba de iniciar el sistema \n")
 f.close()
-while (True):
-    fecha = time.strftime("%Y%m%d")
-    hora = time.strftime("%T")
-    os.system("mosquitto_pub -h 192.168.251.3 -t fecha -m "+fecha)
-    os.system("mosquitto_pub -h 192.168.251.3 -t hora -m "+hora)
-    time.sleep(60)
+sys.exit()
